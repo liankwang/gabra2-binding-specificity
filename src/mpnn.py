8b389df97b9d8bf2a774f8127d22a2ee08bd836a
liankwang <lianwang0713@gmail.com>
2025-03-12 16:52:25 -0400

    Updating GNN

--- src/mpnn.py
+++ src/mpnn.py
@@ -1,175 +1,28 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 
 from rdkit import Chem
+from rdkit.Chem import AllChem
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.data import Data, InMemoryDataset
-from torch_geometric.nn import MessagePassing, GCNConv, global_mean_pool, SAGEConv, GraphConv
+from torch_geometric.nn import MessagePassing, GCNConv, global_mean_pool, SAGEConv, GraphConv,GINEConv
 from torch_geometric.loader import DataLoader
 from torch.utils.data import Subset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, precision_score, recall_score, f1_score, roc_curve, auc, precision_recall_curve
-from torch_geometric.utils import is_undirected
+from torch_geometric.utils import add_self_loops, to_undirected, is_undirected
 from torch.optim.lr_scheduler import StepLR
 
-#from deepchem.feat import MolGraphConvFeaturizer
+from create_graph_dataset import MolGraphDataset
+
 
 random_seed = 42
 torch.manual_seed(random_seed)
 np.random.seed(random_seed)
 
-def get_atom_features(atom):
-    """
-    Get atom features for a single atom, with features based on the MolGraphConv featurizer in deepchem:
-    - Atom type: One-hot vector of length 10 (C, N, O, F, P, S, Cl, Br, I, other)
-    - Formal charge
-    - Hybridization: one-hot vector of length 4 (SP, SP2, SP3, other)
-    - Aromatic: 1 if atom is aromatic, 0 otherwise
-    - Degree: one-hot vector of length 7 (0, 1, 2, 3, 4, 5, other)
-    - Num hydrogens: one-hot vector of length 6 (0, 1, 2, 3, 4, other)
-    """
-    # Atom type: One-hot vector of length 10
-    type_map = {
-        'C': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        'N': [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        'O': [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        'F': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        'P': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        'S': [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-        'Cl': [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-        'Br': [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        'I': [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    }
-    symb = atom.GetSymbol()
-    if symb in ['C', 'N', 'O', 'F', 'P', 'S', 'Cl', 'Br', 'I']:
-        type = type_map[symb]
-    else:
-        type = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-
-
-    # Formal charge
-    charge = [atom.GetFormalCharge()]
-    
-    # Hybridization: one-hot vector of length 4
-    hybridization = atom.GetHybridization()
-    if hybridization == Chem.rdchem.HybridizationType.SP:
-        hybrid = [1, 0, 0, 0]
-    elif hybridization == Chem.rdchem.HybridizationType.SP2:
-        hybrid = [0, 1, 0, 0]
-    elif hybridization == Chem.rdchem.HybridizationType.SP3:
-        hybrid = [0, 0, 1, 0]
-    else:
-        hybrid = [0, 0, 0, 0]
-        print('Hybridization not SP, SP2, or SP3; instead:', hybridization)
-
-    # Hydro gen bonding
-
-    # Aromatic
-    aromatic = [atom.GetIsAromatic()]
-
-    # Degree
-    deg_map = {
-        0: [1, 0, 0, 0, 0, 0, 0],
-        1: [0, 1, 0, 0, 0, 0, 0],
-        2: [0, 0, 1, 0, 0, 0, 0], 
-        3: [0, 0, 0, 1, 0, 0, 0],
-        4: [0, 0, 0, 0, 1, 0, 0],
-        5: [0, 0, 0, 0, 0, 1, 0]
-    }
-    degree = deg_map.get(atom.GetDegree(), [0, 0, 0, 0, 0, 0, 1])
-
-    # Num hydrogens:
-    num_h_map = {0: [1, 0, 0, 0, 0, 0],
-                 1: [0, 1, 0, 0, 0, 0],
-                 2: [0, 0, 1, 0, 0, 0],
-                 3: [0, 0, 0, 1, 0, 0],
-                 4: [0, 0, 0, 0, 1, 0]}
-    num_h = num_h_map.get(atom.GetTotalNumHs(), [0, 0, 0, 0, 0, 1])
-    
-    return np.concatenate([type, charge, hybrid, aromatic, degree, num_h])
-
-def get_bond_features(bond):
-    """Get bond features for a single bond, with features based on the MolGraphConv featurizer in deepchem:
-    - Bond type: one-hot vector of length 5 (single, double, triple, aromatic, other)
-    - Same ring: 1 if bond is in a ring, 0 otherwise
-    - Conjugated: 1 if bond is conjugated, 0 otherwise
-    - Stereo: one-hot vector of length 5 (STEREONONE, STEREOANY, STEREOZ, STEREOE, other)
-    """
-
-    # Bond type: one-hot vector of length 5
-    bond_type = bond.GetBondType()
-    bond_type_map = {
-        Chem.rdchem.BondType.SINGLE: [1, 0, 0, 0, 0],
-        Chem.rdchem.BondType.DOUBLE: [0, 1, 0, 0, 0],
-        Chem.rdchem.BondType.TRIPLE: [0, 0, 1, 0, 0],
-        Chem.rdchem.BondType.AROMATIC: [0, 0, 0, 1, 0],
-    }
-    bond_type_one_hot = bond_type_map.get(bond_type, [0, 0, 0, 0, 1])
-    bond_type_one_hot = bond_type_map[bond_type]
-
-    # Same ring
-    same_ring = bond.IsInRing()
-
-    # Conjugated?
-    conj = bond.GetIsConjugated()
-
-    # Stereo
-    stereo = bond.GetStereo()
-    bond_stereo_map = {
-        Chem.rdchem.BondStereo.STEREONONE: [1, 0, 0, 0, 0],
-        Chem.rdchem.BondStereo.STEREOANY: [0, 1, 0, 0, 0],
-        Chem.rdchem.BondStereo.STEREOZ: [0, 0, 1, 0, 0],
-        Chem.rdchem.BondStereo.STEREOE: [0, 0, 0, 1, 0],
-    }
-    bond_stereo_one_hot = bond_stereo_map.get(stereo, [0, 0, 0, 0, 1])
-
-    return np.concatenate([bond_type_one_hot, [same_ring], [conj], bond_stereo_one_hot])
-    
-def mol_to_graph(mol, label):
-    """Convert an RDKit molecule into a PyTorch Geometric graph object. 
-    Calls atom and bond featurizers.
-     
-    NOTE: try finding pretrained embeddings, or better featurizers. Needs to be bond/atom features, not molecular features
-    """
-    # print(f"Working on mol {Chem.MolToSmiles(mol)} with label {label}")
-    
-    # Compute atom features
-    atom_features = []
-    for atom in mol.GetAtoms():
-        atom_features.append(get_atom_features(atom))
-    x = torch.tensor(atom_features, dtype=torch.float)
-
-    # Compute edge index (adjacency matrix) and edge features
-    edge_index = []
-    edge_attr = []
-    for bond in mol.GetBonds():
-        i = bond.GetBeginAtomIdx()
-        j = bond.GetEndAtomIdx()
-        edge_index.append((i, j))
-        edge_index.append((j, i))  # Undirected Graph
-        edge_attr.append(get_bond_features(bond))
-
-    edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
-    edge_attr = torch.tensor(edge_attr, dtype=torch.float)
-
-    y = torch.tensor([label], dtype=torch.float)
-
-    return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
-
-class MolGraphDataset(InMemoryDataset):
-    """ Creates a PyTorch Geometric dataset from a list of RDKit molecules and labels. """
-    def __init__(self, mols, labels, transform=None, pre_transform=None):
-        self.mols = mols
-        self.labels = labels
-        super(MolGraphDataset, self).__init__('.', transform, pre_transform)
-        data_list = [mol_to_graph(mol, label) for mol, label in zip(mols, labels)]
-        self.data, self.slices = self.collate(data_list)
-
-    def __len__(self):
-        return len(self.mols)
 
 class MPNNLayer(MessagePassing): # NOT IN USE
     """
@@ -238,37 +91,76 @@ class MPNNLayer(MessagePassing): # NOT IN USE
         out = self.W_o(out) # (N x d_h)
         return out
 
-class GraphEncoder(nn.Module):
+class EdgeGCN(MessagePassing):
+    def __init__(self, in_channels, out_channels, edge_dim):
+        super(EdgeGCN, self).__init__(aggr='add')
+        self.lin_nodes = nn.Linear(in_channels, out_channels)
+        self.lin_edges = nn.Linear(edge_dim, out_channels)
+        self.lin = nn.Linear(in_channels *2, out_channels)
+    
+    def forward (self, x, edge_index, edge_attr):
+        # Edge_index has bidirectional edges. Duplicate edge_attr for each direction
+        num_uni_edges = edge_attr.shape[0]
+        edge_attr = edge_attr.repeat(1, 2).reshape(num_uni_edges * 2, edge_attr.shape[1])
+        edge_index, edge_attr = add_self_loops(edge_index, edge_attr)
+
+        #x = self.lin_nodes(x) # Map initial node features to hidden representations
+        return self.propagate(edge_index, x=x, edge_attr=edge_attr)
+
+    def message(self, x_j, edge_attr):
+        dist = edge_attr[:, 0].view(-1, 1)
+        edge_features = self.lin_edges(edge_attr)
+        combined_features = torch.cat([x_j, edge_features], dim=-1)
+        return self.lin(combined_features)
+
+class SimpleGraphEncoder(nn.Module):
     def __init__(self, d_v, d_e, d_h, output_dim):
         print("Initializing GraphEncoder model with dimensions")
         print("Node features:", d_v)
         print("Edge features:", d_e)
         print("Hidden features:", d_h)
-        super(GraphEncoder, self).__init__()
-        self.mpnn1 = MPNNLayer(d_v, d_e, d_h)
-        #self.mpnn2 = MPNNLayer(d_v, d_e, d_h)
-        self.lin = nn.Linear(d_h, output_dim)
+        super(SimpleGraphEncoder, self).__init__()
 
+        self.lin = nn.Linear(d_h, output_dim)
         self.conv1 = GCNConv(d_v, d_h)
         self.conv2 = GCNConv(d_h, d_h)
     
     def forward(self, data):
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
 
-        #x = self.mpnn1(x, edge_index, edge_attr) # outputs (N x d_h)
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         #x = F.dropout(x, p=0.15, training=self.training)
         x = self.conv2(x, edge_index)
-        #x = F.relu(x)
-        # x = self.conv2(x, edge_index)
-        # x = F.relu(x)
 
         # Readout layer; combines information from each node in a given graph
         x = global_mean_pool(x, data.batch)
-        #print("After global mean pool shape:", x.shape)
 
         x = self.lin(x)
+        return x
+
+
+class GraphEncoder(nn.Module):
+    def __init__(self, d_v, d_e, d_h, output_dim):
+        print("Initializing GraphEncoder model with dimensions (dv, de, dh):", d_v, d_e, d_h)
+        super(GraphEncoder, self).__init__()
+
+        self.gcn = GCNConv(d_v, d_h)
+        self.conv1 = EdgeGCN(d_h, d_h, d_e)
+        self.lin = nn.Linear(d_h, output_dim)
+    
+    def forward(self, data):
+        x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+
+        x = self.gcn(x, edge_index)
+        x = F.relu(x)
+        x = self.conv1(x, edge_index, edge_attr)
+        x = F.relu(x)
+        # Readout layer: combines information from each node to give a graph-level representation
+        x = global_mean_pool(x, data.batch)
+
+        x = self.lin(x)
+
         return x
 
 def dataset_to_loaders(dataset, batch_size):
@@ -286,10 +178,56 @@ def dataset_to_loaders(dataset, batch_size):
 
     return train_loader, val_loader, test_loader
 
+class EarlyStopping:
+    def __init__(self, patience=10, verbose=False, delta=0, path='checkpoint.pth'):
+        """
+        Args:
+            patience (int): Number of epochs to wait for improvement before stopping
+            verbose (bool): Print status messages
+            delta (float): Minimum change to qualify as an improvement
+            path (str): Path to save the model checkpoint
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.delta = delta
+        self.path = path
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.best_model_wts = None
+
+    def __call__(self, val_loss, model):
+        """
+        Call this function after each validation epoch to check if training should stop
+        """
+        score = -val_loss  # We want to minimize validation loss, so we negate it
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves the model when validation loss decreases'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.best_score:.6f} --> {val_loss:.6f}).  Saving model...')
+        torch.save(model.state_dict(), self.path)
+        self.best_model_wts = model.state_dict()
+
 def train(model, train_loader, optimizer, scheduler, criterion, num_epochs, val_loader=None, with_val=False):
     model.train()
     train_losses = []
     val_losses = []
+    early_stopping = EarlyStopping(patience=10, verbose=True, path='../output/checkpoint.pth')
 
     for epoch in range(num_epochs):
         total_train_loss = 0
@@ -324,6 +262,11 @@ def train(model, train_loader, optimizer, scheduler, criterion, num_epochs, val_
             ave_val_loss = total_val_loss / len(val_loader)
             val_losses.append(ave_val_loss)
             print(f'Validation Loss: {ave_val_loss:.4f}')
+        
+        # early_stopping(ave_val_loss, model)
+        # if early_stopping.early_stop:
+        #     print("Early stopping")
+        #     break
         
         #scheduler.step()
 
@@ -393,14 +336,13 @@ def evaluate(model, test_loader):
 
     # Calculate accuracy
     accuracy = accuracy_score(true_labels, preds)
-    auroc = roc_auc_score(true_labels, preds)
     print(f'Test Accuracy: {accuracy * 100:.2f}%')
-    print(f'Test AUROC: {auroc * 100:.2f}%')
-    print(f'Test ROC AUC: {roc_auc * 100:.2f}%')
 
     # Compute confusion matrix
     cm = confusion_matrix(true_labels, preds)
     print(f'Confusion Matrix:\n{cm}')
+
+    print(f'AUROC: {roc_auc * 100:.2f}%')
 
     # Calculate precision, recall, f1
     precision = precision_score(true_labels, preds)
@@ -444,20 +386,28 @@ def evaluate(model, test_loader):
     plt.show()
 
 
-
 if __name__ == "__main__":
-    filepath = '../data/processed/iuphar_labeled2.csv'
-    df = pd.read_csv(filepath)
-    smiles_list = df['Smiles'].tolist()
-    labels = torch.tensor(df['Interaction'].tolist(), dtype=torch.long)
+    # filepath = '../data/processed/iuphar_labeled2.csv'
+    # df = pd.read_csv(filepath)
+    # smiles_list = df['Smiles'].tolist()
+    # labels = torch.tensor(df['Interaction'].tolist(), dtype=torch.long)
 
+    # # Calculate weight for positive samples
+    # pos_weight = (labels == 0).float().sum() / (labels == 1).float().sum()
+    # print(f'Positive sample weight: {pos_weight}')
+
+    # # Create dataset of mol objects
+    # mols = [Chem.MolFromSmiles(smiles) for smiles in smiles_list]
+    # dataset = MolGraphDataset(mols, labels)
+
+    dataset = torch.load('../data/processed/iuphar_labeled2.pt')
+    print(f'Loaded dataset with {len(dataset)} samples.')
+
+    # Calculate weight for positive samples
+    labels = dataset.labels
     # Calculate weight for positive samples
     pos_weight = (labels == 0).float().sum() / (labels == 1).float().sum()
     print(f'Positive sample weight: {pos_weight}')
-
-    # Create dataset of mol objects
-    mols = [Chem.MolFromSmiles(smiles) for smiles in smiles_list]
-    dataset = MolGraphDataset(mols, labels)
 
     # Training hyperparameters
     d_v = dataset[0].x.shape[1]
@@ -465,8 +415,8 @@ if __name__ == "__main__":
     d_h = 32
     output_dim = 1
     learning_rate = 1e-3
-    num_epochs = 100
-    batch_size = 8
+    num_epochs = 150
+    batch_size = 16
 
     # Split dataset and convert to PyG DataLoaders
     train_loader, val_loader, test_loader = dataset_to_loaders(dataset, batch_size)
@@ -493,7 +443,10 @@ if __name__ == "__main__":
     plt.ylabel('Loss')
     plt.title('Training and Validation Loss Over Epochs')
     plt.legend()
+    plt.savefig('../output/mpnn_training_curve.png')
     plt.show()
 
     # Evaluating the model
-    evaluate(model, test_loader)
+    start_time = time.time()
+    evaluate(model, val_loader)
+    print(f'Evaluation time: {time.time() - start_time:.2f} seconds')
